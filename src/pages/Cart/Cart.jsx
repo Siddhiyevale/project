4abc4f00fa3b62225aece1d5e23548255d02dbbd
siddhi <siddhiyevale.fit@gmail.com>
2024-10-23 16:28:28 +0530

    first commit

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,38 +1,44 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import { StoreContext } from '../../context/StoreContext';
-import { food_list } from '../../assets/assets';
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
+import { food_list } from "../../assets/assets";
 
 const Cart = () => {
-
-  const {cartItems,food_list,removeFromCart} =useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
-      <div className="cart-items-title">
-        <p>Items</p>
-        <p>Title</p>
-        <p>Price</p>
-        <p>Quantity</p>
-        <p>Total</p>
-        <p>Remove</p>
-        </div>  
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
       </div>
-      <br/>
-      <hr/>
-      {food_list.map((item,index)=>{
-        if(cartItems[item._id]>0)
-        {
+      <br />
+      <hr />
+      {food_list.map((item, index) => {
+        if (cartItems[item._id] > 0) {
           return (
-            <div className='cart-items-title cart items-item'>
-              <p>{item.name}</p>
+            <div>
+              <div className="cart-items-title cart-items-item">
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>{item.price * cartItems[item._id]}</p>
+                <p>x</p>
               </div>
-          )
+              <hr />
+            </div>
+          );
         }
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
